10e178ed64b3851cf8049c6410180ef57a412128
neetac29 <neetapanditchavan94@gmail.com>
2025-08-06 16:08:19 +0530

    added css on personal info

--- client/src/components/Homepages/Header.js
+++ client/src/components/Homepages/Header.js
@@ -113,9 +113,12 @@ const Header = () => {
             {/* single info */}
             <div className="info">
               <label htmlFor="email">Email:</label>
-              <h4>neetapanditchavan29@gmail.com</h4>
+              <h4>
+                <a href="mailto:neetapanditchavan29@gmail.com">
+                  neetapanditchavan29@gmail.com
+                </a>
+              </h4>
             </div>
-
           </div>
 
           <div className="personalInfo-img">
